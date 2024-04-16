c17db04940b7114edc781697d9d55043c0c86988
KrishnaSumanth31 <38893083+KrishnaSumanth31@users.noreply.github.com>
2024-04-16 17:17:09 +0530

    Update main.tf

--- terraform/modules/s3_bucket/main.tf
+++ terraform/modules/s3_bucket/main.tf
@@ -1,5 +1,11 @@
 resource "aws_s3_bucket" "bucket" {
 
-  bucket = var.bucket_name
-  acl = "private"
+  bucket = var.bucket_name_first
+  aws_s3_bucket_acl = "private"
+}
+
+resource "aws_s3_bucket" "bucket-sample" {
+
+  bucket = var.bucket_name_second
+  aws_s3_bucket_acl = "private"
 }
