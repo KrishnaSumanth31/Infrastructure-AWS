dbb819d0907b713b1df955d6c8fc8eade2e8b7c3
KrishnaSumanth31 <38893083+KrishnaSumanth31@users.noreply.github.com>
2024-04-16 17:47:47 +0530

    Update variables.tf

--- terraform/environments/dev/variables.tf
+++ terraform/environments/dev/variables.tf
@@ -3,9 +3,11 @@ variable "region" {
 }
 
 variable "bucket_name_first" {
+  type = string
   description = "Name of the first S3 Bucket"
 }
 
 variable "bucket_name_second" {
+  type = string
   description = "Name of the second S3 Bucket"
 }
