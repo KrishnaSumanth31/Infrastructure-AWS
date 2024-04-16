fb589257b188f5796c429a3fbb0632d926daeaeb
KrishnaSumanth31 <38893083+KrishnaSumanth31@users.noreply.github.com>
2024-04-16 17:40:23 +0530

    Update main.tf

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -5,11 +5,11 @@ provider "aws" {
 module "s3_bucket" {
   source = "../../modules/s3_bucket"
 
-  bucket_name_first = var.bucket_name_first
+  bucket = var.bucket_name_first
   
 }  
 
 module "s3_bucket_two" {
   source = "../../modules/s3_bucket"
-  bucket_name_second = var.bucket_name_second
+  bucket = var.bucket_name_second
 }
