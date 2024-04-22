b4caf95f6c2a24cac15935f2a0598c426ae2b2f1
KrishnaSumanth31 <38893083+KrishnaSumanth31@users.noreply.github.com>
2024-04-22 13:33:57 +0530

    separated the instance and cluster

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -2,26 +2,29 @@ provider "aws" {
   region = var.region
 }
 
-module "postgres" {
-  source = "../../modules/postgres"
-  
-  cluster_identifier             = var.cluster_identifier
-  engine                         = "aurora-postgresql"
-  engine_mode                    = "serverless"
-  database_name                  = var.database_name
-  master_username                = var.master_username
-  master_password                = var.master_password
-  vpc_security_group_ids         = [var.security_group_id]
-  db_subnet_group_name           = var.db_subnet_group_name
-  auto_pause                     = var.auto_pause
-  min_capacity                   = var.min_capacity
-  max_capacity                   = var.max_capacity
-  seconds_until_auto_pause       = var.seconds_until_auto_pause
-  timeout_action                 = var.timeout_action
+module "postgres_cluster" {
+  source                = "../../modules/postgres"
+  region                = var.region
+  cluster_identifier    = var.cluster_identifier
+  database_name         = var.database_name
+  master_username       = var.master_username
+  master_password       = var.master_password
+  security_group_id     = var.security_group_id
+  db_subnet_group_name  = var.db_subnet_group_name
+  auto_pause            = var.auto_pause
+  min_capacity          = var.min_capacity
+  max_capacity          = var.max_capacity
+  seconds_until_auto_pause = var.seconds_until_auto_pause
+  timeout_action        = var.timeout_action
+  tags                  = var.tags
+}
+
+module "aws_rds_cluster_instance" "cluster_instance" {
+  source                = "../../modules/postgres"
   count                  = var.instance_count
+  cluster_identifier     = module.postgres_cluster.cluster_identifier
   instance_class         = var.instance_class
-  identifier             = "${var.cluster_identifier}-instance-${count.index + 1}"
-  tags = var.tags
-    
-}  
+  identifier             = "${module.postgres_cluster.cluster_identifier}-instance-${count.index + 1}"
+}
+
 
