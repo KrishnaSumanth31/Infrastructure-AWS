c419f5ce21b97feb8a4f6429249d9adddefd33ac
KrishnaSumanth31 <38893083+KrishnaSumanth31@users.noreply.github.com>
2024-04-25 15:47:15 +0530

    Update variables.tf

--- terraform/modules/s3_bucket/variables.tf
+++ terraform/modules/s3_bucket/variables.tf
@@ -1,9 +1,9 @@
-variable "bucket_name_first" {
+variable "bucket_name_artifact" {
   type = string
   description = "Name of the first s3 Bucketb"
 }  
 
-variable "bucket_name_second" {
+variable "bucket_name_raw" {
   type = string
   description = "Name of the second s3 Bucketb"
 }
