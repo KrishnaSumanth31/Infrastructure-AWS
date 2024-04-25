6649b2b17b4989d10f95ad0b3b00ae919e41aca9
KrishnaSumanth31 <38893083+KrishnaSumanth31@users.noreply.github.com>
2024-04-25 15:39:25 +0530

    Update main.tf

--- terraform/modules/s3_bucket/main.tf
+++ terraform/modules/s3_bucket/main.tf
@@ -1,11 +1,39 @@
-resource "aws_s3_bucket" "bucket" {
-
-  bucket = var.bucket_name_first
+resource "aws_s3_bucket" "artifact-bucket" {
+  bucket = var.bucket_name_artifact
   acl = "private"
+  versioning {
+    enabled = true
+  }
 }
 
-resource "aws_s3_bucket" "bucket-sample" {
+resource "aws_s3_bucket" "raw-bucket" {
 
-  bucket = var.bucket_name_second
+  bucket = var.bucket_name_raw
   acl = "private"
+
+  lifecycle_rule {
+    enabled = true
+
+    transition {
+      days          = 30
+      storage_class = "INTELLIGENT_TIERING"
+    }
+
+    transition {
+      days          = 60
+      storage_class = "GLACIER"
+    }
+
+    noncurrent_version_transition {
+      days          = 90
+      storage_class = "GLACIER"
+    }
+
+    expiration {
+      days = 365
+    }
+  }
+ object_lock_configuration {
+    object_lock_enabled = "Enabled"
+  }
 }
