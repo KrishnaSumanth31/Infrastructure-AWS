66d8213fc4dca2375b5231f3c8095780803a7f99
KrishnaSumanth31 <38893083+KrishnaSumanth31@users.noreply.github.com>
2024-04-25 16:17:34 +0530

    Update main.tf

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -4,12 +4,7 @@ provider "aws" {
 
 module "artifact_bucket" {
   source     = "../../modules/s3_bucket"
-  bucket_name     = var.bucket_name_artifact  
-}
-
-module "raw_bucket" {
-  source     = "../../modules/s3_bucket"
+  bucket_name     = var.bucket_name_artifact
   bucket_name     = var.bucket_name_raw  
 }
-
 
