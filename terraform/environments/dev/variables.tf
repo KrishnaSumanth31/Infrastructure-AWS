f0f949761b9b4ad7b613b3f91ede81330e3b9870
KrishnaSumanth31 <38893083+KrishnaSumanth31@users.noreply.github.com>
2024-04-25 15:47:22 +0530

    Update variables.tf

--- terraform/environments/dev/variables.tf
+++ terraform/environments/dev/variables.tf
@@ -2,63 +2,12 @@ variable "region" {
   description = "AWS region"
 }
 
-variable "cluster_identifier" {
-  description = "Aurora cluster identifier"
-}
+variable "bucket_name_artifact" {
+  type = string
+  description = "Name of the first s3 Bucketb"
+}  
 
-variable "database_name" {
-  description = "Database name"
-}
-
-variable "master_username" {
-  description = "Master username"
-}
-
-variable "master_password" {
-  description = "Master password"
-}
-
-variable "security_group_id" {
-  description = "ID of the security group for the cluster"
-}
-
-variable "db_subnet_group_name" {
-  description = "Name of the DB subnet group"
-}
-
-variable "auto_pause" {
-  description = "Specifies whether the cluster is paused after a period of inactivity"
-  type        = bool
-}
-
-variable "min_capacity" {
-  description = "The minimum capacity for the Aurora serverless cluster"
-}
-
-variable "max_capacity" {
-  description = "The maximum capacity for the Aurora serverless cluster"
-}
-
-variable "seconds_until_auto_pause" {
-  description = "The amount of time before a paused Aurora serverless cluster is resumed"
-}
-
-variable "timeout_action" {
-  description = "The action to take when the Aurora serverless cluster is paused"
-}
-
-variable "instance_count" {
-  description = "Number of instances in the cluster"
-  type        = number
-  default     = 1
-}
-
-variable "instance_class" {
-  description = "Instance class for the cluster instances"
-  default     = "db.t3.small"
-}
-
-variable "tags" {
-  description = "Tags for the cluster"
-  type        = map(string)
+variable "bucket_name_raw" {
+  type = string
+  description = "Name of the second s3 Bucketb"
 }
