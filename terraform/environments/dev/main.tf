bf92f25407cde7d83bfee94c4e9ffb86b7d4ec61
KrishnaSumanth31 <38893083+KrishnaSumanth31@users.noreply.github.com>
2024-04-25 16:35:21 +0530

    Update main.tf

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -6,4 +6,5 @@ module "s3_bucket" {
   source         = "../../modules/s3_bucket"
   bucket_name_artifact    = var.bucket_name_artifact
   bucket_name_raw   = var.bucket_name_raw
+  region = var.region
 }
