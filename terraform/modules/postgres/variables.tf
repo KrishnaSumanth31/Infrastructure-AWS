ececf4698e2317f6cf511f415ff6a6cfbf8c0bcc
KrishnaSumanth31 <38893083+KrishnaSumanth31@users.noreply.github.com>
2024-04-26 14:26:16 +0530

    Update variables.tf

--- terraform/modules/postgres/variables.tf
+++ terraform/modules/postgres/variables.tf
@@ -2,24 +2,33 @@ variable "region" {
   description = "AWS region"
 }
 
-variable "cluster_identifier" {
-  description = "Aurora cluster identifier"
-}
-
-variable "database_name" {
-  description = "Database name"
-}
-
-variable "master_username" {
-  description = "Master username"
-}
-
-variable "master_password" {
-  description = "Master password"
+variable "vpc_id" {
+  description = "ID of the existing VPC"
 }
 
 variable "security_group_id" {
-  description = "ID of the security group for the cluster"
+  description = "ID of the existing security group"
+}
+
+variable "cluster_identifier" {
+  description = "Identifier for the RDS Aurora cluster"
+}
+
+variable "engine_version" {
+  description = "Version of Aurora PostgreSQL engine"
+  default     = "11.12" # adjust version as needed
+}
+
+variable "database_name" {
+  description = "Name of the database"
+}
+
+variable "master_username" {
+  description = "Username for the master user"
+}
+
+variable "master_password" {
+  description = "Password for the master user"
 }
 
 variable "db_subnet_group_name" {
@@ -27,38 +36,21 @@ variable "db_subnet_group_name" {
 }
 
 variable "auto_pause" {
-  description = "Specifies whether the cluster is paused after a period of inactivity"
-  type        = bool
-}
-
-variable "min_capacity" {
-  description = "The minimum capacity for the Aurora serverless cluster"
+  description = "Whether to enable auto pause"
 }
 
 variable "max_capacity" {
-  description = "The maximum capacity for the Aurora serverless cluster"
+  description = "Maximum capacity of the cluster"
+}
+
+variable "min_capacity" {
+  description = "Minimum capacity of the cluster"
 }
 
 variable "seconds_until_auto_pause" {
-  description = "The amount of time before a paused Aurora serverless cluster is resumed"
-}
-
-variable "timeout_action" {
-  description = "The action to take when the Aurora serverless cluster is paused"
-}
-
-variable "instance_count" {
-  description = "Number of instances in the cluster"
-  type        = number
-  default     = 1
-}
-
-variable "instance_class" {
-  description = "Instance class for the cluster instances"
-  default     = "db.t3.small"
+  description = "Seconds until auto pause"
 }
 
 variable "tags" {
-  description = "Tags for the cluster"
-  type        = map(string)
+  description = "Tags to apply to AWS resources"
 }
