25ccbe54a8c7ebc35460cb9859289dbc00f35487
KrishnaSumanth31 <38893083+KrishnaSumanth31@users.noreply.github.com>
2024-04-26 15:40:23 +0530

    Update variables.tf

--- terraform/modules/postgres/variables.tf
+++ terraform/modules/postgres/variables.tf
@@ -11,18 +11,3 @@ variable "region" {
 }
 
 
-variable "auto_pause" {
-  description = "Whether to enable auto pause"
-}
-
-variable "max_capacity" {
-  description = "Maximum capacity of the cluster"
-}
-
-variable "min_capacity" {
-  description = "Minimum capacity of the cluster"
-}
-
-variable "seconds_until_auto_pause" {
-  description = "Seconds until auto pause"
-}
