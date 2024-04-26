f4d17cb6023da18a9dd514a92c6dac98d960b17d
KrishnaSumanth31 <38893083+KrishnaSumanth31@users.noreply.github.com>
2024-04-26 15:26:33 +0530

    Update variables.tf

--- terraform/modules/postgres/variables.tf
+++ terraform/modules/postgres/variables.tf
@@ -1,39 +1,15 @@
+variable "environment" {
+  description = "The environment name"
+}
+
+variable "cluster_id" {
+  description = "The identifier for the DB cluster"
+}
+
 variable "region" {
-  description = "AWS region"
+  description = "The AWS region"
 }
 
-variable "vpc_id" {
-  description = "ID of the existing VPC"
-}
-
-variable "security_group_id" {
-  description = "ID of the existing security group"
-}
-
-variable "cluster_identifier" {
-  description = "Identifier for the RDS Aurora cluster"
-}
-
-variable "engine_version" {
-  description = "Version of Aurora PostgreSQL engine"
-  default     = "11.12" # adjust version as needed
-}
-
-variable "database_name" {
-  description = "Name of the database"
-}
-
-variable "master_username" {
-  description = "Username for the master user"
-}
-
-variable "master_password" {
-  description = "Password for the master user"
-}
-
-variable "db_subnet_group_name" {
-  description = "Name of the DB subnet group"
-}
 
 variable "auto_pause" {
   description = "Whether to enable auto pause"
@@ -49,8 +25,4 @@ variable "min_capacity" {
 
 variable "seconds_until_auto_pause" {
   description = "Seconds until auto pause"
-}
-
-variable "tags" {
-  description = "Tags to apply to AWS resources"
 }
