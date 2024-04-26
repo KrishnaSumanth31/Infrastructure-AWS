1ebbfaf9c1f25f97c71cae1fed058ef3f5d8fc67
KrishnaSumanth31 <38893083+KrishnaSumanth31@users.noreply.github.com>
2024-04-26 16:42:53 +0530

    Update main.tf

--- terraform/modules/postgres/main.tf
+++ terraform/modules/postgres/main.tf
@@ -19,28 +19,23 @@
 resource "aws_rds_cluster" "postgresql_serverless" {
   cluster_identifier             = var.cluster_id
   engine                         = "aurora-postgresql"
-  engine_mode                    = "provisioned"
+  engine_mode                    = "serverless"
   engine_version                 = "15.2"  # Adjust engine version as per your requirements
   database_name                  = "testdatabaseinfra"
   master_username                = "mydb"
   master_password                = "testmedude"
-  publicly_accessible            = false
-  auto_minor_version_upgrade     = false
-  apply_immediately              = true  # Apply changes immediately
 
   # Serverless V2 configuration
   db_subnet_group_name           = "rdsgroup"
-  db_parameter_group_name        = "aurora-postgresql-parameters"
 
   # Specify existing VPC and subnets
   vpc_security_group_ids         = ["sg-005e6c9357838f2e7"]
-  subnet_ids                     = ["subnet-0ed4bf4f84fd76a0f"]
-  
+
   tags                           = { Environment = var.environment }
 }
 
 resource "aws_rds_cluster_instance" "postgresql_instance" {
-  count                          = "1"
+  count                          = var.instance_count
   identifier                     = "${var.cluster_id}-instance-${count.index}"
   cluster_identifier             = aws_rds_cluster.postgresql_serverless.id
   instance_class                 = "db.r5.large"  # Adjust instance class as per your requirements
@@ -48,6 +43,7 @@ resource "aws_rds_cluster_instance" "postgresql_instance" {
   publicly_accessible            = false
   tags                           = { Environment = var.environment }
 }
+
 
 
 
