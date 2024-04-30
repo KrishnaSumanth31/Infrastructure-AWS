b69f4c4e123c25ec7c221d7cb3eedb3f4d84bc8b
KrishnaSumanth31 <38893083+KrishnaSumanth31@users.noreply.github.com>
2024-04-30 13:31:09 +0530

    Update main.tf

--- terraform/modules/postgres/main.tf
+++ terraform/modules/postgres/main.tf
@@ -91,19 +91,6 @@ resource "aws_rds_cluster_instance" "postgresql_instance" {
   tags                           = { Environment = var.environment }
 }
 
-resource "aws_rds_cluster_instance" "postgresql_instance" {
-  count                          = 1  
-  identifier                     = "${var.cluster_id}-instance-${count.index}"
-  cluster_identifier             = aws_rds_cluster.postgresql_serverless.id
-  instance_class                 = "db.t3.small"  # Adjust instance class as per your requirements
-  engine                         = "aurora-postgresql"
-  publicly_accessible            = false
-  performance_insights_enabled   = true
-  performance_insights_retention_period = 7  # Adjust retention period as needed
-  db_parameter_group_name        = "default.aurora-postgresql15"
-  tags                           = { Environment = var.environment }
-}
-
 output "rds_cluster_endpoint" {
   value = aws_rds_cluster.postgresql_serverless.endpoint
 }
