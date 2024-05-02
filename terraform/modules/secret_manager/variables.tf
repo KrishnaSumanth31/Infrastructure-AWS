a0268c42a124cf780d7ed89d98533ba9bdf0f5bc
KrishnaSumanth31 <38893083+KrishnaSumanth31@users.noreply.github.com>
2024-05-02 19:16:08 +0530

    Create variables.tf

---
+++ terraform/modules/secret_manager/variables.tf
@@ -0,0 +1,9 @@
+variable "username" {
+  description = "The username for the secret"
+  type        = string
+}
+
+variable "password" {
+  description = "The password for the secret"
+  type        = string
+}
