0a648fdada7319872bfb2ffda0c14c5088bf0eec
KrishnaSumanth31 <38893083+KrishnaSumanth31@users.noreply.github.com>
2024-05-02 13:45:19 +0530

    Update main.tf

--- terraform/modules/postgres/main.tf
+++ terraform/modules/postgres/main.tf
@@ -40,17 +40,17 @@
 
 
 #------------------------------------------------------------------------
-resource "aws_secretsmanager_secret" "database_credentials" {
-  name = "database_credentials_devlop"
-}
+#resource "aws_secretsmanager_secret" "database_credentials" {
+#  name = "database_credentials_devlop"
+#}
 
-resource "aws_secretsmanager_secret_version" "database_credentials_version" {
-  secret_id = aws_secretsmanager_secret.database_credentials.id
-  secret_string = jsonencode({
-    username = "mydevdb",
-    password = "Test@me"
-  })
-}
+#resource "aws_secretsmanager_secret_version" "database_credentials_version" {
+#  secret_id = aws_secretsmanager_secret.database_credentials.id
+#  secret_string = jsonencode({
+#    username = "mydevdb",
+#    password = "Test@me"
+#  })
+#}
 
 resource "aws_rds_cluster" "postgresql_serverless" {
   cluster_identifier             = var.cluster_id
@@ -58,8 +58,8 @@ resource "aws_rds_cluster" "postgresql_serverless" {
   engine_mode                    = "provisioned"
   engine_version                 = "15.2"  # Adjust engine version as per your requirements
   database_name                  = "testdatabaseinfra"
-  master_username                = aws_secretsmanager_secret_version.database_credentials_version.secret_string["username"]
-  master_password                = aws_secretsmanager_secret_version.database_credentials_version.secret_string["password"]
+  master_username                = "devuser"
+#  master_password                = aws_secretsmanager_secret_version.rds_credentials_version.secret_string["password"]
   # Serverless V2 configuration
   db_subnet_group_name           = "default"
 
@@ -67,6 +67,17 @@ resource "aws_rds_cluster" "postgresql_serverless" {
   vpc_security_group_ids         = ["sg-005e6c9357838f2e7"]
   deletion_protection            = false
   apply_immediately              = true
+
+  serverlessv2_scaling_configuration = {
+    min_capacity = 2
+    max_capacity = 10
+  }
+
+  instance_class = "db.serverless"
+  instances = {
+    one = {}
+    two = {}
+  }
 
   enabled_cloudwatch_logs_exports = ["postgresql"]
   skip_final_snapshot            = true  # Since deletion_protection is set to false, skipping final snapshot is recommended
@@ -77,18 +88,18 @@ resource "aws_rds_cluster" "postgresql_serverless" {
   tags                           = { Environment = var.environment }
 }
 
-resource "aws_rds_cluster_instance" "postgresql_instance" {
-  count                          = 1  
-  identifier                     = "${var.cluster_id}-instance-${count.index}"
-  cluster_identifier             = aws_rds_cluster.postgresql_serverless.id
-  instance_class                 = "db.r5.large"  # Adjust instance class as per your requirements
-  engine                         = "aurora-postgresql"
-  publicly_accessible            = false
-  performance_insights_enabled   = true
-  performance_insights_retention_period = 7  # Adjust retention period as needed
-  db_parameter_group_name        = "default.aurora-postgresql15"
-  tags                           = { Environment = var.environment }
-}
+#resource "aws_rds_cluster_instance" "postgresql_instance" {
+#  count                          = 1  
+#  identifier                     = "${var.cluster_id}-instance-${count.index}"
+#  cluster_identifier             = aws_rds_cluster.postgresql_serverless.id
+#  instance_class                 = "db.r5.large"  # Adjust instance class as per your requirements
+#  engine                         = "aurora-postgresql"
+#  publicly_accessible            = false
+#  performance_insights_enabled   = true
+#  performance_insights_retention_period = 7  # Adjust retention period as needed
+#  db_parameter_group_name        = "default.aurora-postgresql15"
+#  tags                           = { Environment = var.environment }
+#}
 
 output "rds_cluster_endpoint" {
   value = aws_rds_cluster.postgresql_serverless.endpoint
