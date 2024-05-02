cbeab05c03b6b4576ab7c37efbe38799d3c525f7
KrishnaSumanth31 <38893083+KrishnaSumanth31@users.noreply.github.com>
2024-05-02 19:30:12 +0530

    Update main.tf

--- terraform/modules/secret_manager/main.tf
+++ terraform/modules/secret_manager/main.tf
@@ -4,6 +4,8 @@ provider "aws" {
 
 resource "aws_secretsmanager_secret" "postgres_db_secrets" {
   name = "postgres_db_secrets"
+  description = var.description
+  tags        = var.tags
 }
 
 resource "aws_secretsmanager_secret_version" "postgres_db_secrets_version" {
@@ -13,4 +15,6 @@ resource "aws_secretsmanager_secret_version" "postgres_db_secrets_version" {
     password = var.password,
     port     = var.port
   })
+  description = var.description
+  tags        = var.tags
 }
