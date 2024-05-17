797161f22cf58a1fff2ff60630174f9068d4f799
KrishnaSumanth31 <38893083+KrishnaSumanth31@users.noreply.github.com>
2024-05-17 12:26:49 +0530

    Update main.tf

--- terraform/modules/iam_role/main.tf
+++ terraform/modules/iam_role/main.tf
@@ -40,5 +40,74 @@ resource "aws_iam_role_policy_attachment" "glue_service_notebook_role" {
 
 resource "aws_iam_role_policy_attachment" "glue_secretsmanager_role" {
   role       = aws_iam_role.glue_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/SecretsManagerReadWrite"
+  policy_arn = "arn:aws:iam::aws:policy/SecretsManagerReadWrite"
+}
+
+# Create the crawler role
+resource "aws_iam_role" "crawler_role" {
+  name = "crawler"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "glue.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "crawler_glue_service_role" {
+  role       = aws_iam_role.crawler_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
+}
+
+resource "aws_iam_policy" "custom_policy" {
+  name        = "awsglueservicerole-clin_trl_subject_crawler"
+  description = "Custom policy for Crawler to GetObject and PutObject in S3"
+  policy      = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = [
+          "s3:GetObject",
+          "s3:PutObject"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "custom_policy_attachment" {
+  role       = aws_iam_role.crawler_role.name
+  policy_arn = aws_iam_policy.custom_policy.arn
+}
+
+# Create the secret-role
+resource "aws_iam_role" "secret_role" {
+  name = "secret-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "secretsmanager.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "secret_manager_read_write" {
+  role       = aws_iam_role.secret_role.name
+  policy_arn = "arn:aws:iam::aws:policy/SecretsManagerReadWrite"
 }
