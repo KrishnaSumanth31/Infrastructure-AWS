891da425c98b3a851ac63fc34911efc6f8650b34
KrishnaSumanth31 <38893083+KrishnaSumanth31@users.noreply.github.com>
2024-05-21 16:54:25 +0530

    Update main.tf

--- terraform/modules/s3_bucket/main.tf
+++ terraform/modules/s3_bucket/main.tf
@@ -16,31 +16,13 @@ resource "aws_s3_bucket" "raw_bucket" {
   acl    = "private"
 }
 
-data "aws_iam_policy_document" "artifact_bucket_policy" {
+data "aws_iam_policy_document" "https_only_policy" {
   statement {
     actions   = ["s3:*"]
     effect    = "Deny"
     resources = [
       "arn:aws:s3:::${aws_s3_bucket.artifact_bucket.bucket}",
-      "arn:aws:s3:::${aws_s3_bucket.artifact_bucket.bucket}/*"
-    ]
-    principals {
-      type        = "AWS"
-      identifiers = ["*"]
-    }
-    condition {
-      test     = "Bool"
-      variable = "aws:SecureTransport"
-      values   = ["false"]
-    }
-  }
-}
-
-data "aws_iam_policy_document" "raw_bucket_policy" {
-  statement {
-    actions   = ["s3:*"]
-    effect    = "Deny"
-    resources = [
+      "arn:aws:s3:::${aws_s3_bucket.artifact_bucket.bucket}/*",
       "arn:aws:s3:::${aws_s3_bucket.raw_bucket.bucket}",
       "arn:aws:s3:::${aws_s3_bucket.raw_bucket.bucket}/*"
     ]
@@ -56,13 +38,71 @@ data "aws_iam_policy_document" "raw_bucket_policy" {
   }
 }
 
+data "aws_iam_policy_document" "allow_other_account_policy" {
+  statement {
+    actions   = ["s3:*"]
+    effect    = "Allow"
+    resources = [
+      "arn:aws:s3:::${aws_s3_bucket.artifact_bucket.bucket}",
+      "arn:aws:s3:::${aws_s3_bucket.artifact_bucket.bucket}/*",
+      "arn:aws:s3:::${aws_s3_bucket.raw_bucket.bucket}",
+      "arn:aws:s3:::${aws_s3_bucket.raw_bucket.bucket}/*"
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::123456789012:root"] # Replace with the AWS account ID you want to grant access to
+    }
+  }
+}
+
+data "aws_iam_policy_document" "combined_policy" {
+  statement {
+    actions   = ["s3:*"]
+    effect    = "Deny"
+    resources = [
+      "arn:aws:s3:::${aws_s3_bucket.artifact_bucket.bucket}",
+      "arn:aws:s3:::${aws_s3_bucket.artifact_bucket.bucket}/*",
+      "arn:aws:s3:::${aws_s3_bucket.raw_bucket.bucket}",
+      "arn:aws:s3:::${aws_s3_bucket.raw_bucket.bucket}/*"
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = ["false"]
+    }
+  }
+
+  statement {
+    actions   = ["s3:*"]
+    effect    = "Allow"
+    resources = [
+      "arn:aws:s3:::${aws_s3_bucket.artifact_bucket.bucket}",
+      "arn:aws:s3:::${aws_s3_bucket.artifact_bucket.bucket}/*",
+      "arn:aws:s3:::${aws_s3_bucket.raw_bucket.bucket}",
+      "arn:aws:s3:::${aws_s3_bucket.raw_bucket.bucket}/*"
+    ]
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::123456789012:root"] # Replace with the AWS account ID you want to grant access to
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "artifact_bucket_policy_https" {
+  bucket = aws_s3_bucket.artifact_bucket.id
+  policy = data.aws_iam_policy_document.https_only_policy.json
+}
+
 resource "aws_s3_bucket_policy" "artifact_bucket_policy" {
   bucket = aws_s3_bucket.artifact_bucket.id
-  policy = data.aws_iam_policy_document.artifact_bucket_policy.json
+  policy = data.aws_iam_policy_document.allow_other_account_policy.json
 }
 
 resource "aws_s3_bucket_policy" "raw_bucket_policy" {
   bucket = aws_s3_bucket.raw_bucket.id
-  policy = data.aws_iam_policy_document.raw_bucket_policy.json
+  policy = data.aws_iam_policy_document.combined_policy.json
 }
-
