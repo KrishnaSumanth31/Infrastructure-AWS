51d57afef4a56cc1f593cff06220a88992ad3f36
KrishnaSumanth31 <38893083+KrishnaSumanth31@users.noreply.github.com>
2024-05-21 16:31:44 +0530

    Update main.tf

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -2,12 +2,12 @@ provider "aws" {
   region = var.region
 }
 
-#module "s3_bucket" {
-#  source         = "../../modules/s3_bucket"
-#  bucket_name_artifact    = var.bucket_name_artifact
-#  bucket_name_raw   = var.bucket_name_raw
-#  region = var.region
-#}
+module "s3_bucket" {
+  source         = "../../modules/s3_bucket"
+  bucket_name_artifact    = var.bucket_name_artifact
+  bucket_name_raw   = var.bucket_name_raw
+  region = var.region
+}
 
 
 #module "postgres" {
