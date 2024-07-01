117676ad5204f503963d63dfad73040dd1452bc0
KrishnaSumanth31 <38893083+KrishnaSumanth31@users.noreply.github.com>
2024-07-01 14:37:12 +0530

    Update main.tf

--- terraform/modules/secret_manager/main.tf
+++ terraform/modules/secret_manager/main.tf
@@ -2,14 +2,14 @@ provider "aws" {
   region = "ap-south-1"  # Set your desired AWS region
 }
 
-resource "aws_secretsmanager_secret" "postgres_db_secrets" {
-  name = "postgres_db_secrets_dev"
+resource "aws_secretsmanager_secret" "postgres_db_secrets_demo" {
+  name = "postgres_db_secrets_dev_demo"
   description = var.description
   tags        = var.tags
 }
 
-resource "aws_secretsmanager_secret_version" "postgres_db_secrets_version" {
-  secret_id     = aws_secretsmanager_secret.postgres_db_secrets.id
+resource "aws_secretsmanager_secret_version" "postgres_db_secrets_demo_version" {
+  secret_id     = aws_secretsmanager_secret.postgres_db_secrets_demo.id
   secret_string = <<EOF
   {
   "username": "${var.username}",
