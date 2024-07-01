7d619bbaf8615700a4800777a106820713f47c23
KrishnaSumanth31 <38893083+KrishnaSumanth31@users.noreply.github.com>
2024-07-01 14:25:46 +0530

    Update main.tf

--- terraform/modules/secret_manager/main.tf
+++ terraform/modules/secret_manager/main.tf
@@ -10,9 +10,11 @@ resource "aws_secretsmanager_secret" "postgres_db_secrets" {
 
 resource "aws_secretsmanager_secret_version" "postgres_db_secrets_version" {
   secret_id     = aws_secretsmanager_secret.postgres_db_secrets.id
-  secret_string = jsonencode({
-    username = var.username,
-    password = var.password,
-    port     = var.port
-  })
+  secret_string = <<EOF
+  {
+  "datahub_rsa_key": "${var.datahub_rsa_key}",
+  "datahub_user": "${var.datahub_user}",
+  "datahub_account": "${var.datahub_account}"
+}
+EOF
 }
